a59ab37c1c4b0c1fa26a9f1da31920bcd3a87aee
tauqeer <tauqeer.shakir@yahoo.com>
2016-02-18 13:33:40 +0500

    step 21 completed

--- step21_classess_private_modifier/app.ts
+++ step21_classess_private_modifier/app.ts
@@ -13,3 +13,33 @@ class Animal {
 
 let a : Animal = new Animal("Goat");
 console.log(a);
+
+
+class Ani {
+    private name : string;
+    constructor(theName : string){
+        this.name = theName;
+    }
+}
+class Rhino extends Ani {
+    constructor(){
+        super("Rhino");
+        
+    }
+}
+class Employee {
+    private name : string;
+    constructor(theName : string){
+        this.name = theName;
+    }
+}
+
+let ani : Ani = new Ani("Goat");
+let rhi : Rhino = new Rhino();
+let emp : Employee = new Employee("emp");
+
+// 1 < 0 = true
+ani = rhi;
+ani = emp;      //Both are not equal due to private name property are different in both classess
+
+
